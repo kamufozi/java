5321e6706d14022e40dbb249ab0d6de14b9c85f2
Fozi Chris <kamufozi679@gmail.com>
2025-06-26 19:05:50 +0200

    Added some java exercises

---
+++ simpleApp.java
@@ -0,0 +1,11 @@
+// Create a simple Java file
+public class simpleApp
+{
+    public static void main (String[] args)
+    {
+        int a = 7;
+        int b = 8;
+        int c =a+b;
+        System.out.println("The sum is :"+c);
+    }
+}
---
+++ sumOfTwo.java
@@ -0,0 +1,8 @@
+public class sumOfTwo {
+    public static void main(String[] args){
+        int firstNumber = 20;
+        int secondNumber = 35;
+        int sum = firstNumber + secondNumber;
+        System.out.println("The sum = "+sum);
+    }
+}
